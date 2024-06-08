6bb907d47f497883871ea2405bfb473c8722af5d
anmol <anmols3d@gmail.com>
2024-06-08 14:48:31 +0300

    Column 1 setup completed

--- Assets/Script/OnSelected.cs
+++ Assets/Script/OnSelected.cs
@@ -7,6 +7,7 @@ public class OnSelected : MonoBehaviour
 
     public Animator row1Pos1Animator;
     public Animator fishAnimator;
+    public bool tileActivatedForAnimation = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,16 +17,24 @@ public class OnSelected : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (tileActivatedForAnimation == true)
+        {
+            AnimateSelectedTile();
+            SelectedTileFly();
+            tileActivatedForAnimation = false;
+            Debug.Log("Animation run");
+        }
     }
 
     public void TileOnSelected()
     {
-        fishAnimator.SetBool("Selected", true);
+        tileActivatedForAnimation = true;
+        
     }
     public void SelectedTileFly()
     {
-        StartCoroutine(DelayAfterSelection());
+        //StartCoroutine(DelayAfterSelection());
+        row1Pos1Animator.SetBool("FlytoMatchPos1", true);
     }
 
     IEnumerator DelayAfterSelection() 
@@ -34,5 +43,10 @@ public class OnSelected : MonoBehaviour
 
 
         row1Pos1Animator.SetBool("FlytoMatchPos1", true);
+    }
+
+    private void AnimateSelectedTile()
+    {
+        fishAnimator.SetBool("Selected", true);
     }
 }

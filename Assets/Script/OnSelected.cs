94e8452fd7e46b5bae98ac9f607cafe0afeace48
anmol <anmols3d@gmail.com>
2024-06-08 19:53:10 +0300

    Column 3 setup completed

--- Assets/Script/OnSelected.cs
+++ Assets/Script/OnSelected.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class OnSelected : MonoBehaviour
 {
 
-    public Animator row1Pos1Animator;
-    public Animator fishAnimator;
+    public Animator column1Pos1Animator, column1Pos2Animator,column1Pos3Animator,column1Pos4Animator,column1Pos5Animator,column1Pos6Animator,column1Pos7Animator,column1Pos8Animator;
+    public Animator column2Pos1Animator,column2Pos2Animator,column2Pos3Animator,column2Pos4Animator,column2Pos5Animator,column2Pos6Animator,column2Pos7Animator,column2Pos8Animator;
+    //public Animator column3Pos1Animator,column3Pos2Animator,column3Pos3Animator,column3Pos4Animator,column3Pos5Animator,column3Pos6Animator,column3Pos7Animator,column3Pos8Animator*/
+    //public Animator fishAnimator;
     public bool tileActivatedForAnimation = false;
     // Start is called before the first frame update
     void Start()
@@ -19,34 +21,35 @@ public class OnSelected : MonoBehaviour
     {
         if (tileActivatedForAnimation == true)
         {
-            AnimateSelectedTile();
+            // StartCoroutine(DelayAfterSelection());
             SelectedTileFly();
-            tileActivatedForAnimation = false;
-            Debug.Log("Animation run");
         }
     }
 
     public void TileOnSelected()
     {
         tileActivatedForAnimation = true;
-        
+
+        Debug.Log("tileActivatedForAnimation is now true");
+
     }
     public void SelectedTileFly()
     {
         //StartCoroutine(DelayAfterSelection());
-        row1Pos1Animator.SetBool("FlytoMatchPos1", true);
+        // row1Pos1Animator.SetBool("FlytoMatchPos1", true);
+
+
+       column1Pos1Animator.SetBool("FlytoMatchPos1", true);
+       column1Pos2Animator.SetBool("FlytoMatchPos1", true);
     }
 
     IEnumerator DelayAfterSelection() 
     {
-        yield return new WaitForSeconds(0.15f);
+        //fishAnimator.SetBool("Selected", true);
+        yield return new WaitForSeconds(0.1f);
 
-
-        row1Pos1Animator.SetBool("FlytoMatchPos1", true);
+        
     }
 
-    private void AnimateSelectedTile()
-    {
-        fishAnimator.SetBool("Selected", true);
-    }
+    
 }

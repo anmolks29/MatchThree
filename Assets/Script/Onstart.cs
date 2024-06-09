47052dc93f47786959fc3ab373491327033d95aa
anmol <anmols3d@gmail.com>
2024-06-10 01:19:26 +0300

    Scene loading transition animation added

--- Assets/Script/Onstart.cs
+++ Assets/Script/Onstart.cs
@@ -8,23 +8,26 @@ public class Onstart : MonoBehaviour
     [SerializeField] Animator row2Transition;
     [SerializeField] Animator row3Transition;
     [SerializeField] Animator row4Transition;
+    [SerializeField] Animator col1Transition;
+    [SerializeField] Animator col2Transition;
+    [SerializeField] Animator col3Transition;
     [SerializeField] Animator bgTransition;
     [SerializeField] Animator matchPanelTransition;
     [SerializeField] Animator progressBarTransition;
     // Start is called before the first frame update
-    void Start()
+    
+     void Awake()
     {
-        
-    }
-    private void Awake()
-    {
-        bgTransition.SetBool("BgTransition", true);
+       /* bgTransition.SetBool("BgTransition", true);
         row1Transition.SetBool("Transition", true);
         row4Transition.SetBool("Transition", true);
         row2Transition.SetBool("Transition", true);
         row3Transition.SetBool("Transition", true);
+        col1Transition.SetBool("Col1Transition", true);
+        col2Transition.SetBool("Col2Transition", true);
+        col3Transition.SetBool("Col3Transition", true);
         matchPanelTransition.SetBool("Match3PanelTransition", true);
-        progressBarTransition.SetBool("ProgressBarTransition", true);
+        progressBarTransition.SetBool("ProgressBarTransition", true); */
     }
 
     // Update is called once per frame

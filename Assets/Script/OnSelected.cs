687b1ff15c8b1677f9f8320aa7f72d70e7c588c3
anmol <anmols3d@gmail.com>
2024-06-11 00:22:26 +0300

    Fly Animation setup and initial code added.

--- Assets/Script/OnSelected.cs
+++ Assets/Script/OnSelected.cs
@@ -1,3 +1,4 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,63 +6,87 @@ using UnityEngine;
 public class OnSelected : MonoBehaviour
 {
 
-    public Animator column1Pos1Animator, column1Pos2Animator,column1Pos3Animator,column1Pos4Animator,column1Pos5Animator,column1Pos6Animator,column1Pos7Animator,column1Pos8Animator;
-    //public Animator column2Pos1Animator,column2Pos2Animator,column2Pos3Animator,column2Pos4Animator,column2Pos5Animator,column2Pos6Animator,column2Pos7Animator,column2Pos8Animator;
-    //public Animator column3Pos1Animator,column3Pos2Animator,column3Pos3Animator,column3Pos4Animator,column3Pos5Animator,column3Pos6Animator,column3Pos7Animator,column3Pos8Animator*/
-    //public Animator fishAnimator;
-
-    /*public Animator [] animator;
-    private string matchPos1 = "FlytoMatchPos1";*/
+   
     public bool tileActivatedForAnimation = false;
-    int animatorIndex = 0;
+
+    private TilesAnimator animator;
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
+    
+
     // Update is called once per frame
     void Update()
     {
-        if (tileActivatedForAnimation == true)
+
+        if(tileActivatedForAnimation)
         {
-            // StartCoroutine(DelayAfterSelection());
-            SelectedTileFly();
+            Debug.Log("tileActivatedForAnimation is true. Checking animator.");
+
+            if (animator == null)
+            {
+                animator = TilesAnimator.instance;
+                if (animator == null)
+                {
+                    Debug.LogError("TilesAnimator instance not found in Update!");
+                    return;
+                }
+                else
+                {
+                    Debug.Log("TilesAnimator instance found in Update.");
+                }
+            }
+
+            Debug.Log("Calling AnimateMatchPos1.");
+            animator.AnimateMatchPos1();
+            TileAnimationPlayed();
+            Debug.Log("Animation cycle finished.");
         }
     }
 
     public void TileOnSelected()
     {
+        Debug.Log("TileOnSelected called. Activating tile for animation.");
+
         tileActivatedForAnimation = true;
-        //animator = GetComponent<Animator[]>();
+
+        // Ensure the TilesAnimator is enabled
+        GameObject tilesAnimatorObject = TilesAnimator.instance?.gameObject;
         
+        if (tilesAnimatorObject != null)
+        {
+            if (!tilesAnimatorObject.activeSelf)
+            {
+                Debug.Log("Enabling TilesAnimator GameObject.");
+                tilesAnimatorObject.SetActive(true);
+            }
 
+            animator = TilesAnimator.instance;
+
+            if (animator != null)
+            {
+                Debug.Log("TilesAnimator instance found in TileOnSelected.");
+            }
+            else
+            {
+                Debug.LogError("TilesAnimator instance not found after enabling in TileOnSelected!");
+            }
+        }
+        else
+        {
+            Debug.LogError("TilesAnimator GameObject not found in TileOnSelected!");
+        }
     }
-    public void SelectedTileFly( )
+    public void TileAnimationPlayed()
     {
-        //StartCoroutine(DelayAfterSelection());
-        // row1Pos1Animator.SetBool("FlytoMatchPos1", true);
-
-
-        column1Pos1Animator.SetBool("FlytoMatchPos1", true);
-        column1Pos2Animator.SetBool("FlytoMatchPos1", true);
-        column1Pos3Animator.SetBool("FlytoMatchPos1", true);
-        column1Pos4Animator.SetBool("FlytoMatchPos1", true);
-        column1Pos5Animator.SetBool("FlytoMatchPos1", true);
-        column1Pos6Animator.SetBool("FlytoMatchPos1", true);
-        column1Pos7Animator.SetBool("FlytoMatchPos1", true);
-        column1Pos8Animator.SetBool("FlytoMatchPos1", true);
-        Debug.Log("Animation Played");
-      /* column1Pos2Animator.SetBool("FlytoMatchPos1", true);
-       column1Pos3Animator.SetBool("FlytoMatchPos1", true);*/
+        tileActivatedForAnimation = false;
+        Debug.Log("tileActivatedForAnimation = false");
     }
-
-    IEnumerator DelayAfterSelection() 
-    {
-        //fishAnimator.SetBool("Selected", true);
-        yield return new WaitForSeconds(0.1f);
-
-    }
+    
+   
 
 
 
---
+++ Assets/Script/TilesAnimator.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TilesAnimator : MonoBehaviour
+{
+    public Animator animator;
+
+    public static TilesAnimator instance;
+
+    private void OnEnable()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            animator = GetComponent<Animator>();
+            if (animator == null)
+            {
+                Debug.LogError("Animator component not found on TilesAnimator GameObject!");
+            }
+            else
+            {
+                Debug.Log("TilesAnimator initialized with Animator.");
+            }
+        }
+        else
+        {
+            Debug.LogWarning("Multiple instances of TilesAnimator detected! Destroying duplicate.");
+            
+        }
+
+    }
+
+    public void AnimateMatchPos1()
+    {
+        if (animator != null)
+        {
+            Debug.Log("Setting 'FlytoMatchPos1' to true.");
+            animator.SetBool("FlytoMatchPos1", true);
+            StartCoroutine(TurnAnimationFalse());
+        }
+        else
+        {
+            Debug.LogError("Animator is null when trying to set 'FlytoMatchPos1'!");
+        }
+    }
+
+   IEnumerator TurnAnimationFalse()
+    {
+        yield return new WaitForSeconds(0.4f);
+        animator.SetBool("FlytoMatchPos1", false);
+        Destroy(gameObject);
+    }
+
+
+
+
+}
